5ed44fa684f262a3c4498c17aa0ff374a30bfbe5
Anastasija <anastasija.isajeva@gmail.com>
2015-11-16 15:23:38 +0200

    First commin in 2

---
+++ Git_Console_2/Git_Console_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Git_Console_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Isajeva Anastasija 2nd edition");
+            Console.ReadKey();
+        }
+    }
+}
